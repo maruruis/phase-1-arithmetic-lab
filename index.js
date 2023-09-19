5ce5a2e85396e615ab9b73efee04a035b94b8b52
Ian Kimani <ianmwng@gmail.com>
2023-09-19 08:18:20 +0300

    complete assingment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply = num1* num2;
+
+function randomNumber(){
+    const randomNumber= Math.floor(Math.random()* Number.MAX_SAFE_INTEGER)+1;
+return randomNumber;
+
+}
+const random=randomNumber();
+
+const num3= 10;
+const num4= 6;
+const mod= num3 % num4
+
+const max= Math.max(8,10,12,16,20)
